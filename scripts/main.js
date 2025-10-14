1588a70eb7c71bfd413199279fb2bbf1448db988
Rayane <rayanekanuka@gmail.com>
2025-10-14 15:12:50 +0200

    Changement des commentaires

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,9 @@ let ex = 'ex-1';
 let labyData = data[size][ex];
 
 // Création du labyrinthe
-// Opérateur 'new' fait appel au 'constructor'
 let labyrinthe = new Labyrinthe(labyData);
 
 // On appelle la fonction 'display'
-// qui est dans la classe Labyrinthe
 labyrinthe.display();
 
 console.log("C'est l'entrée",labyrinthe.getStart())
